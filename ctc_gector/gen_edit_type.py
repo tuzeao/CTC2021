71024acb1f3e7e0e76fee037eb5979581c71f5af
tuza <tuza@gitlab.corp.youdao.com>
2022-06-22 10:40:33 +0800

    MOD: bug fix

--- ctc_gector/gen_edit_type.py
+++ ctc_gector/gen_edit_type.py
@@ -4,6 +4,7 @@ from difflib import SequenceMatcher
 def gen_edit_type(source, target):
     matcher = SequenceMatcher(None, source, target)
     diffs = list(matcher.get_opcodes())
+    # print(diffs)
     all_edits = []
 
     for diff in diffs:
@@ -16,7 +17,7 @@ def gen_edit_type(source, target):
                 all_edits.append('$DELETE')
         elif tag == 'insert':
             for i in range(j2-j1):
-                all_edits.append(f"$APPEND_{target[j1:j1+i+1]}")
+                all_edits.append(f"$APPEND_{target[j1+i:j1+i+1]}")
         elif tag == 'replace':
             for i in range(i2-i1):
                 all_edits.append(f"$REPLACE_{target[j1:j1+i+1]}")
@@ -24,8 +25,8 @@ def gen_edit_type(source, target):
 
 
 if __name__ == "__main__":
-    source = "3的5次方是多少"
-    target = "6的3次方是多少"
+    source = "6的7次方是多少"
+    target = "3的5次方是多少"
     print(gen_edit_type(source, target))
     
 
