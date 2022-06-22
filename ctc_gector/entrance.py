d8888a9d160253e841427898617ba793f5b8bec6
tuza <tuza@gitlab.corp.youdao.com>
2022-06-22 15:46:05 +0800

    MOD: bug fix

--- ctc_gector/entrance.py
+++ ctc_gector/entrance.py
@@ -3,6 +3,7 @@ from gector.my_gec_model import GecBERTModel
 from tag_index import index_to_tag, tag_to_index
 from gen_edit_type import gen_edit_type
 import torch
+import json
 
 # ------------------- init --------------------------------------
 model = GecBERTModel(vocab_path='data/output_vocabulary/',
@@ -47,37 +48,69 @@ def gector_predict_single(source, target):
 
     source_tok, target_tok = tokenize(source), tokenize(target)
     all_probs = _gector_predict_single(source_tok, model)
-    edits = gen_edit_type(source, target)
-    edits_index = [tag_to_index.get(e, 16501) for e in edits]  # 加0是为了应对gector的开头cls
+    edits, edits_block = gen_edit_type(source, target)
+    edits_index = [tag_to_index.get(e, 16501) for e in edits]
     all_probs = all_probs[0, 1:, :]
     max_val, max_idx = torch.max(all_probs, dim=-1)
     max_val, max_idx = max_val.tolist(), max_idx.tolist()
-    for i in range(len(source)):
+
+    i = 0
+    while i < len(edits_block):
+        move_flag = True
         word = source[i]
-        edit_op = edits[i]
-        edit_idx = edits_index[i]
-        edit_prob = all_probs[i, edit_idx].tolist()
-        max_id = max_idx[i]
-        max_op = index_to_tag.get(max_id, '@@UNKNOWN@@')
-        max_prob = max_val[i]
+        for edit in edits_block[i]:
+            edit_op = edit
+            edit_idx = tag_to_index.get(edit, tag_to_index['@@UNKNOWN@@'])
+            edit_prob = all_probs[i, edit_idx].tolist()
+            max_id = max_idx[i]
+            max_op = index_to_tag.get(max_id, 'Not Found Tag')
+            max_prob = max_val[i]
+            if edit_op.startswith('$APPEND_') and i == 0:
+                word = "$$BEGIN$$"
+                move_flag = False
+            temp = {}
+            temp['word'] = word
+            temp['edit_op'] = edit_op
+            temp['edit_idx'] = edit_idx
+            temp['edit_prob'] = edit_prob
+            temp['max_op'] = max_op
+            temp['max_idx'] = max_id
+            temp['max_prob'] = max_prob
 
-        temp = {}
-        temp['word'] = word
-        temp['edit_op'] = edit_op
-        temp['edit_idx'] = edit_idx
-        temp['edit_prob'] = edit_prob
-        temp['max_op'] = max_op
-        temp['max_idx'] = max_id
-        temp['max_prob'] = max_prob
+            output['info'].append(temp)
 
-        output['info'].append(temp)
+        if move_flag:
+            i += 1
+
+
+
+    # for i in range(len(source)):
+    #     word = source[i]
+    #     edit_op = edits[i]
+    #     edit_idx = edits_index[i]
+    #     edit_prob = all_probs[i, edit_idx].tolist()
+    #     max_id = max_idx[i]
+    #     max_op = index_to_tag.get(max_id, '@@UNKNOWN@@')
+    #     max_prob = max_val[i]
+    #
+    #     temp = {}
+    #     temp['word'] = word
+    #     temp['edit_op'] = edit_op
+    #     temp['edit_idx'] = edit_idx
+    #     temp['edit_prob'] = edit_prob
+    #     temp['max_op'] = max_op
+    #     temp['max_idx'] = max_id
+    #     temp['max_prob'] = max_prob
+    #
+    #     output['info'].append(temp)
 
     return output
 
 if __name__ == "__main__":
     source = "小萌有5个苹果"
     target = "小明有5个苹果"
-    print(gector_predict_single(source, target))
+    result = gector_predict_single(source, target)
+    print(json.dumps(result, ensure_ascii=False, indent=4))
 
 
 
--- ctc_gector/gen_edit_type.py
+++ ctc_gector/gen_edit_type.py
@@ -11,6 +11,35 @@ def gen_edit_type(source, target):
         tag, i1, i2, j1, j2 = diff
         if tag == 'equal':
             for _ in range(i1, i2):
+                all_edits.append(['$KEEP'])
+        elif tag == 'delete':
+            _temp = []
+            for _ in range(i1, i2):
+                _temp.append('$DELETE')
+            all_edits.append(_temp)
+        elif tag == 'insert':
+            _temp = []
+            for i in range(j2-j1):
+                _temp.append(f"$APPEND_{target[j1+i:j1+i+1]}")
+            all_edits.append(_temp)
+        elif tag == 'replace':
+            _temp = []
+            for i in range(i2-i1):
+                _temp.append(f"$REPLACE_{target[j1+i:j1+i+1]}")
+            all_edits.append(_temp)
+    edits = [y for x in all_edits for y in x]
+    return edits, all_edits
+
+def gen_edit_type_bak(source, target):
+    matcher = SequenceMatcher(None, source, target)
+    diffs = list(matcher.get_opcodes())
+    # print(diffs)
+    all_edits = []
+
+    for diff in diffs:
+        tag, i1, i2, j1, j2 = diff
+        if tag == 'equal':
+            for _ in range(i1, i2):
                 all_edits.append('$KEEP')
         elif tag == 'delete':
             for _ in range(i1, i2):
@@ -21,12 +50,29 @@ def gen_edit_type(source, target):
         elif tag == 'replace':
             for i in range(i2-i1):
                 all_edits.append(f"$REPLACE_{target[j1+i:j1+i+1]}")
-    return all_edits
+    return all_edits, diffs
+
+# def set_range(edits, diffs):
+#     output = []
+#     for diff in diffs:
+#         tag, i1, i2, j1, j2 = diff
+#         if tag == 'equal':
+#             for _ in range(i1, i2):
+#                 output.append(['$KEEP'])
+#         if tag == 'insert':
+#             _temp = []
+#             for i in range(j2-j1):
+#                 _temp.append()
+#                 output.append(f"$APPEND_{target[j1+i:j1+i+1]}")
+
 
 
 if __name__ == "__main__":
     source = "游泳池的平均深度"
-    target = "小明家的游泳池的平均深度"
-    print(gen_edit_type(source, target))
+    target = "游泳池的小明家平均深度"
+    result, diffs = gen_edit_type(source, target)
+    print(len(source), len(result))
+    print(diffs)
+    print(result)
     
 
