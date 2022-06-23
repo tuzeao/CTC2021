0739ece9ba4d92f08ad7e524efc593e499fe61f3
tuza <tuza@gitlab.corp.youdao.com>
2022-06-23 15:51:01 +0800

    MOD: 尝试特殊tranform

--- ctc_gector/token_data.py
+++ ctc_gector/token_data.py
@@ -6,31 +6,41 @@ import os
 
 
 def main(args):
-    if not os.path.exists(args.i):
+    if not os.path.exists(args.i) or not os.path.exists(args.t):
         print("？")
         sys.exit()
     vocab = load_vocab("vocab.txt")
     tokenizer = WordpieceTokenizer(vocab=vocab)
-    with open(args.i, "r", encoding="utf-8") as f, open(args.o, "w", encoding="utf-8") as w:
-        lines = f.readlines()
+    with open(args.s, "r", encoding="utf-8") as fs, open(args.t, "r", encoding="utf-8") as ft, \
+            open(args.so, "w", encoding="utf-8") as ws, open(args.st, "w", encoding="utf-8") as wt:
+        source_lines = fs.readlines()
+        target_lines = ft.readlines()
         count = 0
-        for line in tqdm(lines):
-            line = convert_to_unicode(line).strip()
-            if not line:
+        for s, t in tqdm(zip(source_lines, target_lines)):
+            s = convert_to_unicode(s).strip()
+            t = convert_to_unicode(t).strip()
+            if not s or not t:
                 continue
-            tokens = tokenizer.tokenize(line)
-            if len(tokens) <= 2: continue
-            line = ' '.join(tokens)
-            line = line.replace("##", "")
-            w.write(f"{line}\n")
+            s_tokens = tokenizer.tokenize(s)
+            t_tokens = tokenizer.tokenize(t)
+            if len(s_tokens) <= 3 or len(t_tokens) <= 3: continue
+
+            s_line = ' '.join(s_tokens)
+            t_line = ' '.join(t_tokens)
+            s_line = s_line.replace("##", "")
+            t_line = t_line.replace("##", "")
+            ws.write(f"{s_line}\n")
+            wt.write(f"{t_line}\n")
             count += 1
-        print(f"input: {len(lines)}, output: {count}")
+        print(f"input: {len(source_lines)}, output: {count}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', required=True)
-    parser.add_argument('-o', required=True)
+    parser.add_argument('-s', required=True)
+    parser.add_argument('-t', required=True)
+    parser.add_argument('-so', required=True)
+    parser.add_argument('-to', required=True)
     args = parser.parse_args()
     main(args)
 
