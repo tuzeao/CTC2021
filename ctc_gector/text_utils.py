abc0f100e65eae316f941021bae83d3700a3e43a
tuza <tuza@gitlab.corp.youdao.com>
2022-06-24 18:12:26 +0800

    MOD: bug fix

--- ctc_gector/text_utils.py
+++ ctc_gector/text_utils.py
@@ -1,9 +1,12 @@
 import re
 
 
-def text_process(query):
+def text_process(query: str):
     # 平方立方问题
     query = query.replace('m²', 'm2').replace('m³', 'm3')
     query = query.replace("m^3", "m3").replace("m^2", "m2")
+    query = query.replace("\n", "")
+    query = query.replace(" ", "").replace("\xa0", "")
+
 
     return query
