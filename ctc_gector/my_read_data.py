df4a588eb5ed151f151fb753eefa832088214caf
tuza <tuza@gitlab.corp.youdao.com>
2022-06-28 12:00:27 +0800

    MOD: 新数据上传

--- ctc_gector/my_read_data.py
+++ ctc_gector/my_read_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import argparse
-
+from text_utils import text_process
 
 def main(args):
     DATA_PATH = args.i
@@ -11,6 +11,8 @@ def main(args):
     with open(args.s, "w", encoding="utf-8") as fs, open(args.t, "w", encoding="utf-8") as ft:
         for info in df:
             t1, t2 = info[0], info[1]
+            if "SUBQBODY" in t1 or "SUBQBODY" in t2: continue
+            t1, t2 = text_process(t1), text_process(t2)
             fs.write(t2+"\n")
             ft.write(t1+"\n")
 
--- ctc_gector/text_utils.py
+++ ctc_gector/text_utils.py
@@ -3,6 +3,7 @@ import re
 
 def text_process(query: str):
     # 平方立方问题
+    query = query.strip()
     query = query.replace('m²', 'm2').replace('m³', 'm3')
     query = query.replace("m^3", "m3").replace("m^2", "m2")
     query = query.replace("\n", "")
